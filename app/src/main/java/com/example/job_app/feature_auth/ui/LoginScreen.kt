cfafe4eeb5be31ab3aeda2c1757825864767c308
vgn40 <victornylev@hotmail.dk>
2024-04-26 14:16:22 +0200

    0.5

--- app/src/main/java/com/example/job_app/feature_auth/ui/LoginScreen.kt
+++ app/src/main/java/com/example/job_app/feature_auth/ui/LoginScreen.kt
@@ -2,24 +2,16 @@ package com.example.job_app.feature_auth.ui
 import androidx.compose.foundation.Image
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,11 +20,9 @@ import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.times
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.compose.rememberNavController
 import com.example.job_app.R
 import com.example.job_app.feature_auth.viewmodel.LoginViewModel
 import com.example.job_app.ui.theme.JobappTheme
@@ -71,8 +61,8 @@ fun LoginScreen(
         ) {
             // Place your logo image here
             Image(
-                painter = painterResource(id = R.drawable.ic_launcher_foreground),
-                modifier = Modifier.size(190.dp),
+                painter = painterResource(id = R.drawable.bird),
+                modifier = Modifier.size(200.dp),
                 contentDescription = "Logo"
             )
         }
@@ -94,7 +84,7 @@ fun LoginScreen(
             shape = RoundedCornerShape(24.dp),
             colors = TextFieldDefaults.outlinedTextFieldColors(
                 focusedTextColor = Color.White,
-                unfocusedTextColor = Color.Black,
+                unfocusedTextColor = Color.LightGray,
                 disabledTextColor = Color.Gray,
                 cursorColor = Color.White,
                 focusedBorderColor = Color.White,
@@ -118,14 +108,14 @@ fun LoginScreen(
             shape = RoundedCornerShape(24.dp),
             colors = TextFieldDefaults.outlinedTextFieldColors(
                 focusedTextColor = Color.White,
-                unfocusedTextColor = Color.Black,
-                disabledTextColor = Color.Gray,
+                unfocusedTextColor = Color.LightGray,
+                disabledTextColor = Color.White,
                 cursorColor = Color.White,
                 focusedBorderColor = Color.White,
                 unfocusedBorderColor = Color.White,
                 disabledBorderColor = Color.Transparent,
-                focusedLabelColor = Color.Gray,
-                unfocusedLabelColor = Color.Gray
+                focusedLabelColor = Color.White,
+                unfocusedLabelColor = Color.White
             ),
             visualTransformation = PasswordVisualTransformation()
         )
@@ -136,9 +126,9 @@ fun LoginScreen(
             modifier = Modifier
                 .padding(horizontal = padding)
                 .height(48.dp)
-                .width(300.dp)
+                .width(275.dp)
         ) {
-            Text("Login", color = Color(0xFF1565C0))
+            Text("Login", color = Color(0xFF1565C0),style = MaterialTheme.typography.titleLarge.copy(fontSize = 25.sp))
         }
             Spacer(modifier = Modifier.height(16.dp))
 
@@ -147,10 +137,10 @@ fun LoginScreen(
             modifier = Modifier
                 .padding(horizontal = padding * 2)
                 .height(48.dp)
-                .width(300.dp)
+                .width(275.dp)
 
         ) {
-            Text("Register", color = Color(0xFF1565C0))
+            Text("Register", color = Color(0xFF1565C0),style = MaterialTheme.typography.titleLarge.copy(fontSize = 20.sp ))
         }
     }
 
--- app/src/main/java/com/example/job_app/ui/theme/Type.kt
+++ app/src/main/java/com/example/job_app/ui/theme/Type.kt
@@ -2,19 +2,21 @@ package com.example.job_app.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.job_app.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily(Font(R.font.mclaren_regular)),
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
