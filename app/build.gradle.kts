1341bcba9bd2d98d398fc03c292c66138cd4c1e4
vgn40 <victornylev@hotmail.dk>
2024-05-05 10:49:59 +0200

    Running app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,8 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.google.firebase:firebase-bom:VERSION")
+    implementation ("com.google.firebase:firebase-analytics-ktx")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
--- app/src/main/java/com/example/job_app/MainActivity.kt
+++ app/src/main/java/com/example/job_app/MainActivity.kt
@@ -1,5 +1,11 @@
 package com.example.job_app
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,26 +13,59 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.job_app.navigation.Navigation
 import com.example.job_app.ui.theme.JobappTheme
-import com.google.firebase.Firebase
-import com.google.firebase.initialize
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
+
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        Firebase.initialize(this)
-        super.onCreate(savedInstanceState)
-        setContent {
 
+    private val CHANNEL_ID = "app_exit_notifications"
+    private val notificationId = 101
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Firebase.initialize(this)
+        createNotificationChannel()
+        sendNotification()
+        setContent {
             JobappTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Navigation()
                 }
             }
         }
+    }
+
+    private fun createNotificationChannel() {
+        val name = "OnAppStart"
+        val descriptionText = "This channel is for start functionality"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
+        val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    private fun sendNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU && ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.POST_NOTIFICATIONS), PERMISSION_REQUEST_CODE)
+        } else {
+            val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Hello")
+                .setContentText("World")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            NotificationManagerCompat.from(this).notify(notificationId, builder.build())
+        }
+    }
+
+    companion object {
+        private const val PERMISSION_REQUEST_CODE = 123
     }
 }
